152a86d85b10926f2a8bee3c163358fe8bfabd63
fabianruiz <nfrc293@gmail.com>
2024-06-25 20:31:12 +0200

    webApplication: modificado puerto, userRestController y añadido userDto

--- src/main/java/com/fabianr/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/fabianr/webapp/springboot_web/controllers/UserRestController.java
@@ -1,7 +1,7 @@
 package com.fabianr.webapp.springboot_web.controllers;
 
 import com.fabianr.webapp.springboot_web.models.User;
-import org.springframework.ui.Model;
+import com.fabianr.webapp.springboot_web.models.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +13,20 @@ import java.util.Map;
 @RequestMapping("/api")
 public class UserRestController {
     @GetMapping("/details")
-    public Map<String, Object> details() {
+    public UserDto details() {
+        UserDto userDto = new UserDto();
         User user = new User("Fabian", "Ruiz");
-        Map<String, Object> body = new HashMap<>();
-
-        body.put("title", "Hola mundo spring boot!!!!!!!!");
-        body.put("user", user);
-        return body;
+        userDto.setUser(user);
+        userDto.setTitle("Hola mundo spring boot!!!!!!!!");
+        return userDto;
     }
 }
+
+//@GetMapping("/details")
+//public Map<String, Object> details() {
+//    User user = new User("Fabian", "Ruiz");
+//    Map<String, Object> body = new HashMap<>();
+//    body.put("title", "Hola mundo spring boot!!!!!!!!");
+//    body.put("user", user);
+//    return body;
+//}
